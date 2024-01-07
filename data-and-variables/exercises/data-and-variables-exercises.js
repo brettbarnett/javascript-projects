26a611f093dda43b07788e2d7630e153f35814c2
Brett Barnett <brett_barnett@sbcglobal.net>
2024-01-06 21:46:18 -0600

    Calculate trip to mars and the moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,27 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let spaceShuttleName = "Determination";
+let shuttleSpeed = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384400;
+const milesPerKilometer = 0.621;
+
+console.log("Type of spaceShuttleName: " + typeof spaceShuttleName);
+console.log("Type of shuttleSpeed: " + typeof shuttleSpeed);
+console.log("Type of distanceToMars: " + typeof distanceToMars);
+console.log("Type of distanceToMoon: " + typeof distanceToMoon);
+console.log("Type of milesPerKilometer: " + typeof milesPerKilometer);
+
+let milesToMars = distanceToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeed;
+let daysToMars = hoursToMars / 24;
+
+console.log(spaceShuttleName, "will take", daysToMars, "days to reach Mars.");
+
+let milesToMoon = distanceToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeed;
+let daysToMoon = hoursToMoon / 24;
+
+console.log(spaceShuttleName, "will take", daysToMoon, "days to reach the Moon.");

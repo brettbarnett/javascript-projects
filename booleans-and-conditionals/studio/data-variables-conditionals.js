59a21cb280ece6dac75666b92b0a546b3a1a82ec
Brett Barnett <brett_barnett@sbcglobal.net>
2024-01-11 20:55:18 -0600

    Shuttle Launch Studio 1/11/2024

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,64 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+const maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
+//let preparedForLiftOff = true;
+let preparedForLiftOff = false;
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+if (astronautCount <= 7)
+{
+    if (astronautStatus === "ready")
+    {
+        if (totalMassKg < maximumMassLimit)
+        {
+            if (fuelTempCelsius >= minimumFuelTemp && fuelTempCelsius <= maximumFuelTemp)
+            {
+                if (fuelLevel === "100%")
+                {
+                    if (weatherStatus === "clear")
+                    {
+                        preparedForLiftOff = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (preparedForLiftOff)
+{
+    console.log("All systems are! Initiating space shuttle launch sequence.");
+    console.log("-----------------------------------------------------------");
+    console.log("Date:", date);
+    console.log("Time:", time);
+    console.log("Astronaut Count:", astronautCount);
+    console.log("Crew Mass:", crewMassKg);
+    console.log("Fuel Mass:", fuelMassKg);
+    console.log("Shuttle Mass:", shuttleMassKg);
+    console.log("Total Mass:", totalMassKg);
+    console.log("Fuel Temperature:", fuelTempCelsius);
+    console.log("Weather Status:", weatherStatus);
+    console.log("-----------------------------------------------------------");
+    console.log("Have a safe trip astronauts!");
+
+}
+else
+{
+    console.log("Aborting mission!");
+}
 
 // add logic below to verify all astronauts are ready
 

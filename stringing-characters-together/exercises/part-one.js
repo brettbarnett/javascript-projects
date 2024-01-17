1ba04151103dd1ba5cdd6ad0e59ca8f0c87cadf9
Brett Barnett <brett_barnett@sbcglobal.net>
2024-01-16 21:48:34 -0600

    Completed stringing-characters-together exercises

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,24 @@
-let num = 1001;
+let num = 1001.55;
+let numStr = String(num);
 
 //Returns 'undefined'.
-console.log(num.length);
+//console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+//let numStr = String(num);
+//console.log(`${numStr} has ${numStr.length} characters`);
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+//let decimal = 100.50;
+//let decimalStr = String(decimal);
+//console.log(`${decimalStr} has ${decimalStr.length - 1} digits`);
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (numStr.indexOf('.') > -1)
+{
+    console.log(`${numStr} contains ${numStr.length - 1} digits`);
+}
+else
+{
+    console.log(`${numStr} contains ${numStr.length} digits`);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,28 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+let languageSlice = language.slice(0, 1) + language.slice(4, 5);
+console.log(languageSlice);
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+let languageInitials = language.charAt(0) + language.charAt(4);
+console.log(languageInitials);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+let phrase = `The abbreviation for ${language} is ${languageInitials}.`;
+console.log(phrase);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+console.log(phrase.toUpperCase().trim());
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let firstWord = notTitleCase.split(' ')[0];
+let secondWord = notTitleCase.split(' ')[1];
+firstWordUppercase = firstWord[0].toUpperCase() + firstWord.substring(1);
+secondWordUppercase = secondWord[0].toUpperCase() + secondWord.substring(1);
+let titleCase = firstWordUppercase + ' ' + secondWordUppercase;
+console.log(titleCase);

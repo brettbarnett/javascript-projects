699533f69e3d0db921a9014ce113cbd1f155a497
Brett Barnett <brett_barnett@sbcglobal.net>
2024-02-08 20:38:14 -0600

    Completed Class 9 Studio Unit Testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,39 @@
-
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployee: 100,
+    programsOffered: ["Web Development", "Data Analysis", "LiftOff"],
+    LaunchOutput: function(num) {
+        let output = '';
+
+        if (num % 2 === 0) {
+            output += 'Launch';
+        }
+
+        if (num % 3 === 0) {
+            output += 'Code';
+        }
+
+        if (num % 5 === 0) {
+            if (output) {
+                output += 'Rocks';
+            }
+            else {
+                output += 'Rocks';
+            }
+        }
+
+        if (!output) {
+            return "Rutabagas! That doesn't work.";
+        }
+        else if (output === 'Launch Rocks') {
+            return output + ' ! Smack!!!!';
+        }
+        else {
+            return output + '!';
+        }
+    }
 }
+console.log(launchcode.LaunchOutput(6));
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,31 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
   // Write your unit tests here!
-  
+  test("Should ouput Launch! when given a number only divisible by 2", function() {
+    expect(launchcode.LaunchOutput(8)).toBe('Launch!');
+  });
+  test("Should ouput Code! when given a number only divisible by 3", function() {
+    expect(launchcode.LaunchOutput(3)).toBe('Code!');
+  });
+  test("Should ouput Rocks! when given a number only divisible by 5", function() {
+    expect(launchcode.LaunchOutput(25)).toBe('Rocks!');
+  });
+  test("Should ouput LaunchCode! when given a number divisible by 2 and 3", function() {
+    expect(launchcode.LaunchOutput(6)).toBe('LaunchCode!');
+  });
+  test("The value for the organization key should be nonprofit",() => {
+    expect(launchcode['organization']).toBe('nonprofit');
+  });
+  test("The value for the executiveDirector key should be 'Jeff'",() => {
+    expect(launchcode.executiveDirector).toBe('Jeff');
+  });
+  test("The value for the percentageCoolEmployees key should be 100",() => {
+    expect(launchcode.percentageCoolEmployee).toBe(100);
+  });
+  test("The value for the programsOffered key should be ['Web Development', 'Data Analysis', 'LiftOff']",() => {
+    expect(launchcode.programsOffered).toContain('Web Development');
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("LiftOff");
+  });
 });

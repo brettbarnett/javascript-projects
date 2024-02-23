0b51ea91c29411875e58798f5e77f2b498bd3cae
Brett Barnett <brett_barnett@sbcglobal.net>
2024-02-23 10:20:42 -0600

    Completed Chapter 23 Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,36 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+  // Put your code for the exercises here.
+  button.addEventListener("click", () => {
+    paragraph.innerHTML = "Houston, we have liftoff!";
+  });
+
+  button.addEventListener("mouseover", () => {
+    button.style = "background-color: green";
+  });
+
+  button.addEventListener("mouseleave", () => {
+    button.style = "";
+  });
+
+  missionAbort.addEventListener("mouseover", () => {
+    missionAbort.style = "background-color: red";
+  });
+
+  missionAbort.addEventListener("mouseleave", () => {
+    missionAbort.style = "";
+  });
+
+  missionAbort.addEventListener("click", () => {
+    let abortConfirmed = window.confirm("Are you sure you want to abort the mission?");
     
+    if (abortConfirmed) {
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    }
+  });
 }
 
 window.addEventListener("load", init);
